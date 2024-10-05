c23b366e93e6fdbaf81d5dd773a72bcd6e5b120c
Janjanjan1 <janeetbaj@gmail.com>
2024-10-05 12:20:04 -0400

    nothing burger

--- lfs.sh
+++ lfs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# Our Mount Point
+# Mount Point
 export LFS=/mnt/lfs
 export LFS_TGT=x86_64-linux-gnu
 # Plugin device and get the device name from sudo dmesg
+# replace * with the device name 
 export LFS_DISK=/dev/*
 
-# Check if it is mounted and if it is not then start the setup process.
+# Check if it is mounted and if it is not then start the setup process so that we can appropriately mount the device.
 if ! grep -q "$LFS" /proc/mounts; then
 	sorurce setupdisk.sh $LFS_DISK
 fi
