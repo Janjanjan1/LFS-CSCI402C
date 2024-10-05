30ddc6e6dd3a821bc38955e00dc0afc4188a7f8c
Janeet Bajracharya <janeetbaj@gmail.com>
2024-10-05 13:09:39 -0400

    making partitions for root, home and boot

--- lfs.sh
+++ lfs.sh
@@ -3,10 +3,10 @@
 export LFS=/mnt/lfs
 export LFS_TGT=x86_64-linux-gnu
 # Plugin device and get the device name from sudo dmesg
-# replace * with the device name 
-export LFS_DISK=/dev/*
+# replace * with the device name
+export LFS_DISK=/dev/sdb
 
 # Check if it is mounted and if it is not then start the setup process so that we can appropriately mount the device.
 if ! grep -q "$LFS" /proc/mounts; then
-	sorurce setupdisk.sh $LFS_DISK
+	source setupdisk.sh $LFS_DISK
 fi
--- setupdisk.sh
+++ setupdisk.sh
@@ -1,24 +1,30 @@
 
-#if var are sourced then we will just inherit it from LSF.sh. 
+#if var are sourced then we will just inherit it from LSF.sh.
 LSF_DISK="$1"
 
 # within the EOFs all your commands go in and it will input it sequentially
+# use command g to make GPT partition Table
+# make 1G for boot. 40G for Root and rest for home
 sudo fdisk "$LSF_DISK" << EOF
-o
-a
+g
 n
-p
 1
 
-+100M
++1G
+
+
 n
-p
-a
 2
+
++40G
+
+
+n
+3
+
 
 
 
 p
 w
-q
 EOF
